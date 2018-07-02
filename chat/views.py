53db2df93bed22a63fcbb9fd6f441d1b74a557d5
rubinstory <rubinstory@naver.com>
2018-07-02 18:23:11 +0900

    chatbot

--- chat/views.py
+++ chat/views.py
@@ -2,48 +2,49 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 import json
- 
- 
- 
+
+
+
 def keyboard(request):
- 
+    
     return JsonResponse({
-        'type':'buttons',
-        'buttons':['Today','Tommorrow']
-    })
- 
+                        'type':'buttons',
+                        'buttons':['오늘','내일']
+                        })
+
 @csrf_exempt
 def answer(request):
- 
+    
     json_str = ((request.body).decode('utf-8'))
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
- 
-    if datacontent == 'Today':
-        today = "Today's meal"
- 
+    
+    if datacontent == '오늘':
+        today = "오늘 급식"
+        
         return JsonResponse({
-                'message': {
-                    'text': today
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['Today','Tommorrow']
-                }
- 
-            })
- 
-    elif datacontent == 'Tommorrow':
-        tomorrow = "Tommorow's meal"
- 
+                            'message': {
+                            'text': today
+                            },
+                            'keyboard': {
+                            'type':'buttons',
+                            'buttons':['오늘','내일']
+                            }
+                            
+                            })
+    
+    elif datacontent == '내일':
+        tomorrow = "내일 급식"
+        
         return JsonResponse({
-                'message': {
-                    'text': tomorrow
-                },
-                'keyboard': {
-                    'type':'buttons',
-                    'buttons':['Today','Tommorrow']
-                }
- 
-            })
- 
+                            'message': {
+                            'text': tomorrow
+                            },
+                            'keyboard': {
+                            'type':'buttons',
+                            'buttons':['오늘','내일']
+                            }
+                            
+                            })
+
+
