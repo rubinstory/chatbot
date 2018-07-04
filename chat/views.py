18103c54aafb56b4e6a2530f9f979bcde5bfbb26
rubinstory <rubinstory@naver.com>
2018-07-04 23:58:35 +0900

    chatbot

--- chat/views.py
+++ chat/views.py
@@ -25,11 +25,11 @@ def message(request):
     
     num = int(rand.random() * 100) % 30 + 1
 
-    if ((num >= 1) && (num <= 10)):
+    if ((num >= 1) and (num <= 10)):
         choose = '가위'
-    elif ((num >= 11) && (num <= 20)):
+    elif ((num >= 11) and (num <= 20)):
         choose = '바위'
-    elif ((num >= 21) && (num <= 30)):
+    elif ((num >= 21) and (num <= 30)):
         choose = '보'
 
     result = "당신의 수 : {}\n컴퓨터의 수 : {}\n".format(datacontent,choose)
