ce7bfc989f52ebf5ca7a65d81c10d2fd120317ad
rubinstory <rubinstory@naver.com>
2019-03-14 01:50:00 +0900

    openpyxl added

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 import json
-import random as rand
+import openpyxl
 
 ftp_password = 'wnff.6308.2'
 doorlock_password = '1234'
@@ -47,6 +47,35 @@ def message(request):
     
     else: #이름이 입력된 경우
         entered_name = datacontent
+
+        # 엑셀파일 열기
+        wb = openpyxl.load_workbook('list.xlsx')
+
+        # 현재 Active Sheet 얻기
+        ws = wb.active
+ 
+        for row in ws.rows:
+            index = 0
+            team_num = row[index].value
+            index += 1
+            while index <= (len(row) - 1):
+                list_name = row[index].value
+                if list_name == entered_name:
+                    response = 'bb'
+                    wb.close()
+                        return JsonResponse({
+                        'message': {
+                        'text': response
+                        },
+                        'keyboard': {
+                        'type':'buttons',
+                        'buttons':['청소조 확인', 'FTP서버 비밀번호 확인', '동방 비밀번호 확인']
+                        }
+                        })
+                else:
+                    index += 1
+
+        wb.close()
     
     
     
