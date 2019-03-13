08c7a8ab1f5729b59b14c78edbc555f0f9798ab4
rubinstory <rubinstory@naver.com>
2019-03-14 00:45:24 +0900

    t

--- chat/views.py
+++ chat/views.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse, HttpResponse
 import json
 import random as rand
 
+ftp_password = 'wnff.6308.2'
+doorlock_password = '1234'
 
 def index(request):
     return HttpResponse("This is Beta Bot.")
@@ -12,8 +14,8 @@ def index(request):
 def keyboard(request):
     
     return JsonResponse({
-                            'type':'buttons',
-                            'buttons':['가위','바위','보']
+                        'type':'buttons',
+                        'buttons':['청소조 확인', 'FTP서버 비밀번호 확인', '동방 비밀번호 확인']
                         })
 
 @csrf_exempt
@@ -23,57 +25,26 @@ def message(request):
     received_json_data = json.loads(json_str)
     datacontent = received_json_data['content']
     
-    num = int(rand.random() * 100) % 30 + 1
-
-    if ((num >= 1) and (num <= 10)):
-        choose = '가위'
-    elif ((num >= 11) and (num <= 20)):
-        choose = '바위'
-    elif ((num >= 21) and (num <= 30)):
-        choose = '보'
-
-    result = "당신의 수 : {}\n컴퓨터의 수 : {}\n".format(datacontent,choose)
-
-    if datacontent == '가위':
-        if choose == '가위':
-            result += '사용자와 컴퓨터가 비겼습니다.'
-
-        elif choose == '바위':
-            result += '컴퓨터가 이겼습니다.'
-
-        elif choose == '보':
-            result += '사용자가 이겼습니다.'
-
-
-    elif datacontent == '바위':
-        if choose == '바위':
-            result += '사용자와 컴퓨터가 비겼습니다.'
-
-        elif choose == '보':
-            result += '컴퓨터가 이겼습니다.'
-
-        elif choose == '가위':
-            result += '사용자가 이겼습니다.'
-
-    elif datacontent == '보':
-        if choose == '보':
-            result += '사용자와 컴퓨터가 비겼습니다.'
-
-        elif choose == '가위':
-            result += '컴퓨터가 이겼습니다.'
-
-        elif choose == '바위':
-            result += '사용자가 이겼습니다.'
-
+    result = 'nothing'
+    
+    if datacontent == '청소조 확인':
+    
+    elif datacontent == 'FTP서버 비밀번호 확인':
+        result = ftp_password
+    else:
+        result = doorlock_password
+    
+    
+    
     return JsonResponse({
-                            'message': {
-                                'text': result
-                            },
-                            'keyboard': {
-                                'type':'buttons',
-                                'buttons':['가위','바위','보']
-                            }
-
+                        'message': {
+                        'text': result
+                        },
+                        'keyboard': {
+                        'type':'buttons',
+                        'buttons':['청소조 확인', 'FTP서버 비밀번호 확인', '동방 비밀번호 확인']
+                        }
+                        
                         })
 
 
