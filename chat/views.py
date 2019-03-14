32eec5ac27383f74c5d5d85cf23de124f150482c
rubinstory <rubinstory@naver.com>
2019-03-14 11:14:26 +0900

    import sys added

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse, HttpResponse
 import json
+import sys
+sys.path.append('/usr/local/lib/python3.6/dist-packages')
 import openpyxl
 
 ftp_password = 'wnff.6308.2'
